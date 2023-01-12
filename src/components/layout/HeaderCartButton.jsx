a3ea0fccf4f7e4875acb60a2b690943caf6ab627
Aryan Upadhyay <aryan.offc@gmail.com>
2023-01-13 00:32:03 +0530

    Modifying numberOfItems badge in the HeaderCartButton component.

--- src/components/layout/HeaderCartButton.jsx
+++ src/components/layout/HeaderCartButton.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import CartIcon from "../cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
+    const cart = useContext(CartContext);
+
+    const numberOfItems = cart.items.reduce((prev, item) => prev + item.amount, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -11,7 +16,7 @@ const HeaderCartButton = props => {
             <span>
                 Your Cart
             </span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfItems}</span>
         </button>
     );
 }
